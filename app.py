bc3ef2174743874f3a1a0113a66473e703ffa023
LinuXminds <linu@datazoom.com>
2023-12-14 15:25:54 +0530

    change

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask('hello-world')
 @app.route('/')
 def hello():
- return "This is a funccssaa returning this from github actions merged from PR branch bla bla!!!\n"
+ return "This is returning this from github actions merged from PR!!!\n"
 if __name__ == '__main__':
  app.run(host = '0.0.0.0', port = 8080)
